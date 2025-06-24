f649af503d183824ea079e6d9004379444a1ad92
ShovalShabi <shovalshabi@gmail.com>
2025-06-25 01:42:58 +0300

    Fine-tuned validations and auto assignment by MySQL Server

--- PlayerService/src/main/java/org/example/dtos/PlayerDTO.java
+++ PlayerService/src/main/java/org/example/dtos/PlayerDTO.java
@@ -43,7 +43,8 @@ public class PlayerDTO {
     private List<@NotBlank(message = "Position name must not be blank") String> positions;
 
     @NotNull(message = "Height must be provided")
-    @Positive(message = "Height must be a positive number")
+    @Min(value = (long) 1.5, message = "The player is too short to play")
+    @Max(value = (long) 2.2, message = "The player is too tall to play")
     private Double height; // Measured with Meters
 
     private Date creationDate; //Optional on POST request
@@ -100,8 +101,8 @@ public class PlayerDTO {
                 positionEntities,
                 dto.getDateOfBirth(),
                 dto.getHeight(),
-                dto.getCreationDate(),
-                dto.getLastModifiedDate()
+                null,
+                null
         );
     }
 }
