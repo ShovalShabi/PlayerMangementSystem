314d931b3066c91e2ee3d8e4909c9ffd234a9bc1
ShovalShabi <shovalshabi@gmail.com>
2025-06-25 00:11:03 +0300

    Basic service configuration

---
+++ PlayerService/src/main/java/org/example/etc/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package org.example.etc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile({ "dev", "test" })
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Player Management Service")
+                        .version("v1.0"));
+    }
+}
