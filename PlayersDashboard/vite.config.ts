8fdc88c54ec9ea91974bdd9e2b4a989b4831f41d
ShovalShabi <shovalshabi@gmail.com>
2025-06-26 16:29:02 +0300

    Condigured env retrieval

--- PlayersDashboard/vite.config.ts
+++ PlayersDashboard/vite.config.ts
@@ -1,24 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import getEnvVariables from "./src/etc/load-env-variables";
-import dotenv from "dotenv";
 
-dotenv.config({ path: ".env.${import.meta.env.VITE_ENV}" });
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-const { port } = getEnvVariables();
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: port,
-  },
-  define: {
-    "process.env": {
-      VITE_PORT: process.env.VITE_PORT,
-      VITE_ENV: process.env.VITE_ENV,
-      VITE_BACKEND_HOST: process.env.VITE_BACKEND_HOST,
-      VITE_BACKEND_PORT: process.env.VITE_BACKEND_PORT,
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      // make sure to coerce to number if needed
+      port: Number(env.VITE_PORT),
     },
-  },
-});
+    define: {
+      // expose ALL loaded env vars under process.env...
+      "process.env": env,
+    },
+  });
+};
