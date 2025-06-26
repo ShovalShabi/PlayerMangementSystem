aef9be9b1e49a203a708975d76867f131a987874
ShovalShabi <shovalshabi@gmail.com>
2025-06-26 23:41:25 +0300

    Created data types for filters

---
+++ PlayersDashboard/src/utils/interfaces/filters.ts
@@ -0,0 +1,19 @@
+interface Filters {
+  firstName: string;
+  lastName: string;
+  nationality: string;
+  minAge: string;
+  maxAge: string;
+  minHeight: string;
+  maxHeight: string;
+  positions: string[];
+  rowsPerPage: number;
+}
+
+interface UseDebouncedFiltersProps {
+  initialFilters: Filters;
+  delay?: number;
+  onFiltersChange: (filters: Filters) => void;
+}
+
+export type { Filters, UseDebouncedFiltersProps };
