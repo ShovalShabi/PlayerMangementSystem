501bb78a07140065dedaea5f12186a4e0e7889de
ShovalShabi <shovalshabi@gmail.com>
2025-06-27 10:50:39 +0300

    Integrated redux persist with application

--- PlayersDashboard/src/main.tsx
+++ PlayersDashboard/src/main.tsx
@@ -4,19 +4,23 @@ import App from "./App";
 import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 import { themeSettings } from "./theme";
 import { Provider, useSelector } from "react-redux";
-import store, { RootState } from "./store";
+import store, { persistor } from "./store";
 import { AlertProvider } from "./components/AlertProvider";
+import { PersistGate } from "redux-persist/integration/react";
+import { State } from "./utils/interfaces/state";
 
 const ThemedApp = () => {
-  const mode = useSelector((state: RootState) => state.theme.mode);
+  const mode = useSelector((state: State) => state.theme);
   const theme = React.useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AlertProvider>
-        <App />
-      </AlertProvider>
-    </ThemeProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AlertProvider>
+          <App />
+        </AlertProvider>
+      </ThemeProvider>
+    </PersistGate>
   );
 };
 
