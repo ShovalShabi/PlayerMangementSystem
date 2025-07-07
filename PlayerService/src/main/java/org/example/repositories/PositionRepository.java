6999c05988f95360223de49baed2684f0ad6c4e5
ShovalShabi <shovalshabi@gmail.com>
2025-07-07 21:16:05 +0300

    Applied find position if not exisit create mechnism, mapped the keys to be the actual positions for preventing redundency

--- PlayerService/src/main/java/org/example/repositories/PositionRepository.java
+++ PlayerService/src/main/java/org/example/repositories/PositionRepository.java
@@ -1,13 +1,33 @@
 package org.example.repositories;
 
 import org.example.entities.PositionEntity;
+import org.example.utils.enums.Positions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Repository interface for Position entities.
  * Provides CRUD operations and custom queries for Position data.
  */
 @Repository
-public interface PositionRepository extends JpaRepository<PositionEntity, Long> {
+public interface PositionRepository extends JpaRepository<PositionEntity, Positions> {
+
+    /**
+     * Find a position by its enum value or create a new one if it doesn't exist.
+     * 
+     * @param position the position enum value
+     * @return the existing or newly created PositionEntity
+     */
+    default PositionEntity findOrCreate(Positions position) {
+        Optional<PositionEntity> existing = findById(position);
+        if (existing.isPresent()) {
+            return existing.get();
+        } else {
+            PositionEntity newEntity = new PositionEntity();
+            newEntity.setPosition(position);
+            return save(newEntity);
+        }
+    }
 }
